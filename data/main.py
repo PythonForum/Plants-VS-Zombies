443fc9edaf2043c164a7b4822c3013d8f2b416a2
metulburr <metulburr@gmail.com>
2013-06-27 13:04:35 -0400

    changed spacing from tabs to spaces

--- data/main.py
+++ data/main.py
@@ -2,25 +2,25 @@
 import pygame
 
 class Control:
-	def __init__(self):
-		pygame.init()
-		self.screensize = (400,400)
-		self.screen = pygame.display.set_mode(self.screensize)
-		pygame.display.set_caption('Plants VS Zombies')
-		self.clock = pygame.time.Clock()
-		self.gamestate = True
-		self.mainloop()
-		
-	def update(self):
-		pass 
-		
-	def mainloop(self):
-		while self.gamestate:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					self.gamestate = False
-			self.update()
-			pygame.display.flip()
-			
+    def __init__(self):
+        pygame.init()
+        self.screensize = (400,400)
+        self.screen = pygame.display.set_mode(self.screensize)
+        pygame.display.set_caption('Plants VS Zombies')
+        self.clock = pygame.time.Clock()
+        self.gamestate = True
+        self.mainloop()
+        
+    def update(self):
+        pass 
+        
+    def mainloop(self):
+        while self.gamestate:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.gamestate = False
+            self.update()
+            pygame.display.flip()
+            
 if __name__ == '__main__':
-	app = Control()
+    app = Control()
